6499e3cc6ac46c9b87a93b7512723ca37878bcb7
Vinícius Marrara Banaco <vinibanaco@gmail.com>
2022-01-18 16:15:33 -0300

    fix(api): set allowed origins to WebSockets server

--- api/src/socket-connections.js
+++ api/src/socket-connections.js
@@ -4,10 +4,15 @@
 
 const { Server } = require('socket.io');
 
+const config = require('./config');
 const game = require('./modules/game');
 
 module.exports = async (server) => {
-  const io = new Server(server);
+  const io = new Server(server, {
+    cors: {
+      origin: config.get('allowedOrigins'),
+    },
+  });
 
   io.on('connection', (socket) => {
     game.registerSocketHandlers(io, socket);
