6fe2a761ec5d668c0518ed32fca307bf8fdbf486
Vinícius Marrara Banaco <vinibanaco@gmail.com>
2022-01-20 18:43:06 -0300

    chore(web): display user's name on Header

--- web/components/header/index.tsx
+++ web/components/header/index.tsx
@@ -1,18 +1,23 @@
+import { useContext } from 'react';
 import Image from 'next/image';
 
 import HStack from '../h-stack';
 
 import styles from './index.module.css';
 import logoFull from '../../public/logos/logo-full.svg';
+import UserTag from '../user-tag';
+import { UserContext } from '../../contexts/user';
 
 export default function Header() {
+  const { user } = useContext(UserContext);
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <HStack justify="space-between" align="center" style={{ height: '100%' }}>
           <Image src={logoFull} width="175" height="48" alt="Scrum Voting logo" />
 
-          <div />
+          <div>{user.name ? <UserTag name={user.name} /> : null}</div>
         </HStack>
       </div>
     </div>
